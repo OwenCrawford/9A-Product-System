e6a20aac047b544f3d81d3b2593da351bb603268
Owen Crawford <iknowrobot@gmail.com>
2024-12-01 00:33:02 -0600

    DB setup

--- sql/DBSetup.sql
+++ sql/DBSetup.sql
@@ -1,6 +1,6 @@
 DROP TABLE Inventory;
-DROP TABLE Order;
 DROP TABLE OrderParts;
+DROP TABLE Orders;
 DROP TABLE ShippingCharges;
 
 /*
@@ -20,22 +20,22 @@ CREATE TABLE Inventory (
     quantity INT
 );
 
-CREATE TABLE Order (
+CREATE TABLE Orders (
     orderNum INT PRIMARY KEY AUTO_INCREMENT,
     timePlaced TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status CHAR(10),
     totalPrice DECIMAL(9,2)
 );
 
-CREATE TABLE OrderPart (
+CREATE TABLE OrderParts (
     orderNum INT NOT NULL,
     partNum INT NOT NULL,
     quantity INT NOT NULL,
 
-    FOREIGN KEY(orderNum) REFERENCES Order(orderNum)
+    FOREIGN KEY(orderNum) REFERENCES Orders(orderNum)
 );
 
-CREATE TABLE ShippingCharge (
+CREATE TABLE ShippingCharges (
     bracketName CHAR(15) PRIMARY KEY,
     weightCutoff DECIMAL(5,2),
     charge DECIMAL(4,2)
@@ -46,16 +46,16 @@ INSERT INTO Inventory VALUES(2, 22);
 INSERT INTO Inventory VALUES(3, 33);
 INSERT INTO Inventory VALUES(4, 44);
 
-INSERT INTO Order VALUES (1, DEFAULT, "placed", 111.11);
-INSERT INTO Order VALUES (2, DEFAULT, "authorized", 222.22);
-INSERT INTO Order VALUES (3, DEFAULT, "shipped", 333.33);
+INSERT INTO Orders VALUES(1, DEFAULT, "placed", 111.11);
+INSERT INTO Orders VALUES(2, DEFAULT, "authorized", 222.22);
+INSERT INTO Orders VALUES(3, DEFAULT, "shipped", 333.33);
 
-INSERT INTO OrderPart VALUES (1, 1, 1);
-INSERT INTO OrderPart VALUES (1, 2, 2);
-INSERT INTO OrderPart VALUES (2, 3, 3);
-INSERT INTO OrderPart VALUES (3, 4, 4);
+INSERT INTO OrderParts VALUES(1, 1, 1);
+INSERT INTO OrderParts VALUES(1, 2, 2);
+INSERT INTO OrderParts VALUES(2, 3, 3);
+INSERT INTO OrderParts VALUES(3, 4, 4);
 
-INSERT INTO ShippingCharge ("LOW", 5.0, 5.00);
-INSERT INTO ShippingCharge ("MEDIUM", 15.0, 10.00);
-INSERT INTO ShippingCharge ("HIGH", 50.0, 15.00);
-INSERT INTO ShippingCharge ("SUPER", 500.0, 25.00);
+INSERT INTO ShippingCharges VALUES("LOW", 5.0, 5.00);
+INSERT INTO ShippingCharges VALUES("MEDIUM", 15.0, 10.00);
+INSERT INTO ShippingCharges VALUES("HIGH", 50.0, 15.00);
+INSERT INTO ShippingCharges VALUES("SUPER", 500.0, 25.00);
